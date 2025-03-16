fd82fbae26b3ace0ad68087d00480d2eacf8fe03
kchenqi <kchenqi@uci.edu>
2025-03-16 23:47:27 +0000

    I added the word ladder, following the pseudocode.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -1,18 +1,39 @@
 #include "ladder.h"
-#include <stdexcept>
-#include <fstream>
 
 void error(string word1, string word2, string msg) {
 	cerr << "Error: " << msg << word1 << " -> " << word2 << endl;
 }
 
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d) {
+	
 }
 
 bool is_adjacent(const string& word1, const string& word2) {
+	return edit_distance_within(word1, word2, 1);
 }
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+	queue<vector<string>> ladder_queue;
+	ladder_queue.push({begin_word});
+	set<string> visited;
+	visited.insert(begin_word);
+	
+	while (!ladder_queue.empty()) {
+		vector<string> ladder = ladder_queue.front();
+		ladder_queue.pop();
+		string last_word = ladder.back();
+		for (string word : word_list) {
+			if (is_adjacent(last_word, word) && visited.find(word) == visited.end()) {
+				visited.insert(word);
+				vector<string> new_ladder = ladder;
+				new_ladder.push_back(word);
+				visited.insert(word);
+				if (word == end_word) return new_ladder;
+				ladder_queue.push(new_ladder);
+			}
+		}
+	}
+	return {};
 }
 
 void load_words(set<string> & word_list, const string& file_name) {
