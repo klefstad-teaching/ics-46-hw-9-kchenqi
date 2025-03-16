9e775c243d3f7129ec91358c4e8b5eabca6c8932
kchenqi <kchenqi@uci.edu>
2025-03-16 21:29:22 +0000

    I added the methods I will need to implement. I will work on Dijkstra's Algorithm using the pseudocode and the slides.

---
+++ src/dijkstras.cpp
@@ -0,0 +1,21 @@
+#include "dijkstras.h"
+
+struct Node {
+	int vertex;
+	int weight;
+}
+
+
+
+vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
+	int numVertices = G.numVertices;
+	
+}
+
+vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination) {
+	
+}
+
+void print_path(const vector<int>& v, int total) {
+	
+}
