b941444e926b45a5e7bfa0d1ce715e5eafbc8307
kchenqi <kchenqi@uci.edu>
2025-03-17 01:06:10 +0000

    I am debugging my code.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -57,6 +57,10 @@ void load_words(set<string> & word_list, const string& file_name) {
 }
 
 void print_word_ladder(const vector<string>& ladder) {
+	if (ladder.empty() {
+		cout << "No word ladder found." << endl;
+		return;
+	cout << "Word ladder found: ";
 	for (size_t i = 0; i < ladder.size(); ++i) {
 		if (i != ladder.size() - 1) 
 			cout << ladder[i] << " -> ";
