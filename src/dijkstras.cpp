948ebd0adaaebabdf21c75af24e17d575d40a271
kchenqi <kchenqi@uci.edu>
2025-03-17 01:00:19 +0000

    I am debugging my code.

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -48,7 +48,7 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 }
 
 void print_path(const vector<int>& v, int total) {
-	cout << "Total cost is " << total << endl;
 	for (size_t i = 0; i < v.size(); ++i)
-		cout << v[i];
+		cout << v[i] << " ";
+	cout << "Total cost is " << total << endl;
 }
